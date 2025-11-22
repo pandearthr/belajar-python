4be9ef04219af364c1eb823f019566b148fff02b
pandearthr <gede.arthur18@gmail.com>
2025-11-22 14:55:17 +0700

    resume section added

---
+++ UTS BPD/9 a.py
@@ -0,0 +1,13 @@
+# Inisialisasi nilai awal
+nilai = 1
+
+# Mencetak nilai awal
+print(nilai, end=" ")
+
+# Menggunakan loop untuk menghasilkan deret angka
+for _ in range(10):  # 10 adalah jumlah angka dalam deret
+    nilai *= 2
+    print(nilai, end=" ")
+
+# Mencetak baris baru setelah deret selesai
+print()
